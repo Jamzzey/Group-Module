138494ef3deebcbfb33c33c2d7118600fdcecead
monkeyman0121 <51915426+monkeyman0121@users.noreply.github.com>
2019-07-16 14:54:16 +0100

    not pushed

---
+++ Assets/Scripts/Emission Control.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
